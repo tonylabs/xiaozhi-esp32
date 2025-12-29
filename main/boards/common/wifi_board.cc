a6eb12ea184e43420e6c26055638b295e192a039
TONYLABS <tony.wang@tonylabs.com>
2025-12-29 12:49:06 +0800

    Change SSID name

--- main/boards/common/wifi_board.cc
+++ main/boards/common/wifi_board.cc
@@ -54,7 +54,7 @@ void WifiBoard::StartNetwork() {
 
     // Initialize WiFi manager
     WifiManagerConfig config;
-    config.ssid_prefix = "Xiaozhi";
+    config.ssid_prefix = "Stikadoo";
     config.language = Lang::CODE;
     wifi_manager.Initialize(config);
 
